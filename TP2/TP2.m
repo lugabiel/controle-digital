36f054ae821b28dfc2f93e068fe587b8c113bf67
lugabiel <36977837+lugabiel@users.noreply.github.com>
2021-03-25 19:13:46 -0300

    final commit

--- TP2/TP2.m
+++ TP2/TP2.m
@@ -1,38 +1,14 @@
 %% gerando modelo em espaco da planta de 2a ordem a partir
 %  da funcao de transferencia de malha aberta do processo.
-
 num = [1];
 den = [0.3 1 0];
 Ts = tf(num,den)
 Ts = zpk(Ts)
 Tz = c2d(Ts,0.01,'zoh')
-
-% definindo modelo em espaco de estados
+%% definindo modelo em espaco de estados
 Tz = zpk(Tz)
 [zNum, zDen] = tfdata(Ts,'v')
-
-%%
 [A,B,C,D] = tf2ss(num,den)
-p = [0.9 -1.0];
+%% controle por alocacao de polos
+p = [0.9 -.5];
 K = place(A,B,p)
-
-%definindo matriz para contrla usando metodo LQR
-%Q = [0.001 0;
-%     0 1];
-%Q = Q
-%R = 1;
-%K = lqr(A,B,Q,R)
-
-%A = A - B*K
-%[At,Bt,Ct,Dt,Et] = ss((A - B*K),B,C,D)
-%bode(A,B,C,D);
-
-%%
-
-%t = [0:0.1:5]';
-%[y, t, x] = step(sys,t);
-%for i=1:3
-%  subplot(3,1,i); 
-%  plot(t, x(:,i));  % look at how I indexed into x 
-%end 
-
