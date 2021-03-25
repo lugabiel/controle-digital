c5541e60b5328433a757dab53e2ef8d48b68239d
Pedro <pedroianmoraes@gmail.com>
2021-03-25 14:27:43 -0300

    Update TP2.m

--- TP2/TP2.m
+++ TP2/TP2.m
@@ -8,19 +8,20 @@ Tz = c2d(Ts,0.01,'zoh')
 
 % definindo modelo em espaco de estados
 Tz = zpk(Tz)
-[zNum, zDen] = tfdata(Tz,'v')
+[zNum, zDen] = tfdata(Ts,'v')
 
+%%
 [A,B,C,D] = tf2ss(zNum,zDen)
 
 %definindo matriz para contrla usando metodo LQR
 Q = [1 0;
      0 1];
-R = 1
-K = lqrd(A,B,Q,R);
-[At,Bt,Ct,Dt,Et] = ss((A - B*K),B,C,D,0.01)
-
-
-
+R = 0.8;
+K = lqr(A,B,Q,R);
+%[At,Bt,Ct,Dt,Et] = ss((A - B*K),B,C,D)
+A = A - B*K;
+bode(A,B,C,D)
+%%
 
 %t = [0:0.1:5]';
 %[y, t, x] = step(sys,t);
