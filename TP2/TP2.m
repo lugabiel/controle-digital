ca22fb9787eb74bce15ec5e0e9b73e2ef6e5d150
lugabiel <36977837+lugabiel@users.noreply.github.com>
2021-03-25 18:15:39 -0300

    saída do controlador em espaco de estados
    reposta do modelo TP_varying_antigo para uma entrada em degrau

--- TP2/TP2.m
+++ TP2/TP2.m
@@ -3,7 +3,8 @@
 
 num = [1];
 den = [0.3 1 0];
-Ts = tf(num,den);
+Ts = tf(num,den)
+Ts = zpk(Ts)
 Tz = c2d(Ts,0.01,'zoh')
 
 % definindo modelo em espaco de estados
@@ -11,16 +12,21 @@ Tz = zpk(Tz)
 [zNum, zDen] = tfdata(Ts,'v')
 
 %%
-[A,B,C,D] = tf2ss(zNum,zDen)
+[A,B,C,D] = tf2ss(num,den)
+p = [0.9 -1.0];
+K = place(A,B,p)
 
 %definindo matriz para contrla usando metodo LQR
-Q = [1 0;
-     0 1];
-R = 0.8;
-K = lqr(A,B,Q,R);
+%Q = [0.001 0;
+%     0 1];
+%Q = Q
+%R = 1;
+%K = lqr(A,B,Q,R)
+
+%A = A - B*K
 %[At,Bt,Ct,Dt,Et] = ss((A - B*K),B,C,D)
-A = A - B*K;
-bode(A,B,C,D)
+%bode(A,B,C,D);
+
 %%
 
 %t = [0:0.1:5]';
